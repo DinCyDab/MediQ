429467806fd5ad102372d6c8c53e5906db1fb781
DinCyDab <19103291@usc.edu.ph>
2025-04-30 10:26:11 +0800

    Initial commit

---
+++ MediQ/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MediQ
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MediQ/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MediQ;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
